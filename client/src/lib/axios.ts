102629089e5ab1881b2d504e7c90948296977c0b
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-12-14 10:56:27 +0530

    update axios instance to remove fallback base URL for production

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8080/api',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
